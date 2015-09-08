d5135bf593839da022f67983f4aea9556f98df8b
MKooga <martinkooga@gmail.com>
2015-09-08 21:39:23 +0300

    Kujundus CSS [Delivers102663332]

---
+++ index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="Stylesheet.css">
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<img id="pilt" src="assets/img/Ottpilt.jpg">
+<div id="esimene">
+    <h1>Ott-Eriku tutvustus</h1>
+    <!--Otti poolt antud tekst-->
+    <p>
+        Ott-Erik on 20-aastane noormees Tartust. Peale keskkooli lõpetamist asus ta aega teenima kaitseväes, Kuperjanovi jalaväepataljonis. Peale seda töötas ühe aasta ning nüüd asus õppima Tartu Kutsehariduskeskusesse noorem tarkvaraarendaja erialale.
+        Tema üheks suuremaks hobiks on filmimine ning videomontaaž. Ta on sellega tegelenud pea 10 aastat, kuid tõsisemalt viimased 4 aastat. Põhilised videod, mida ta teeb on reklaam-, muusika- ning pulmavideod.
+        Teiseks hobiks ja huviks on Otil kujunenud kodulehtede tegemine ning veebidisain. Ta ei pea ennast sellel alal veel üldse kogenuks, kuid soovib antud valdkonda endale selgemaks saada.<br><hr><p> Sellest aitab.
+    </p>
+</div>
+
+<div id="toidud">
+    <h2>Lemmiktoidud</h2>
+    <ul>
+        <li>Hamburger</li>
+        <li>Pasta</li>
+        <li>Barbecue</li>
+    </ul>
+</div>
+<div id="sport">
+    <h2>Lemmikspordialad</h2>
+    <ol>
+        <li>Triatlon</li>
+        <li>Korvpall</li>
+        <li>Jalgpall</li>
+    </ol>
+</div>
+<div id="stats">
+    <table>
+        <th;>Character sheet</th>
+        <tr>
+            <td>Strength</td>
+            <td>Endurance</td>
+            <td>Perception</td>
+            <td>Intelligence</td>
+            <td>Luck</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>9</td>
+            <td>6</td>
+            <td>5</td>
+            <td>6</td>
+        </tr>
+    </table>
+</div>
+<div id="end">
+    <a href="www.google.ee">Google</a>
+    <p>Sest miks ka <span>mitte</span>.</p>
+</div>
+</body>
+</html>
